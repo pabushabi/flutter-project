f3535ddc82d088625b570ab255ce444c66ad742e
Pavel Shalimov <pabushabi1@gmail.com>
2026-04-27 16:04:07 +0300

    fix deps

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -3,17 +3,17 @@ import 'package:mason_hooks_flutter/mason_hooks_flutter.dart';
 
 void run(HookContext context) {
   addDependencies(context, dependencies: [
-    'flutter_bloc',
-    'bloc',
-    'get_it',
-    'google_fonts',
-    'shared_preferences',
-    'freezed',
-    'go_router',
-    'freezed_annotation',
+    'flutter_bloc: ^9.1.1',
+    'bloc: ^9.2.0',
+    'get_it: ^9.2.1',
+    'google_fonts: ^8.0.2',
+    'shared_preferences: ^2.5.5',
+    'freezed: ^3.2.5',
+    'go_router: ^17.2.2',
+    'freezed_annotation: 3.1.0',
   ], devDependencies: [
-    'build_runner',
-    'flutter_lints',
-    'flutter_launcher_icons',
+    'build_runner: ^2.14.1',
+    'flutter_lints: ^6.0.0',
+    'flutter_launcher_icons: ^0.14.4',
   ]);
 }
